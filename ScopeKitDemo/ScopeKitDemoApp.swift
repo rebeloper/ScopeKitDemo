d14e7a9cd9ea20905f7b984dfb75551cc5f70838
rebeloper <therebeloper@gmail.com>
2026-05-25 20:41:34 +0200

    Initial Commit

---
+++ ScopeKitDemo/ScopeKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScopeKitDemoApp.swift
+//  ScopeKitDemo
+//
+//  Created by Alex Nagy on 25.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct ScopeKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
