f905811cb25630f599b06ceb4712a08501152b39
rebeloper <therebeloper@gmail.com>
2026-05-26 18:39:48 +0200

    finished

--- ScopeKitDemo/ContentView.swift
+++ ScopeKitDemo/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import ScopeKit
 
 struct ContentView: View {
     @State private var observableViewModel = ObservableViewModel()
@@ -29,10 +30,27 @@ struct ContentView: View {
 struct ObservableView: View {
     var body: some View {
         VStack {
+            ScopeView(ObservableViewModel.self) { viewModel, _ in
+                Button {
+                    viewModel.count += 1
+                    print("Did tap Observable: \(viewModel.count)")
+                } label: {
+                    Text(viewModel.text)
+                        .foregroundStyle(.white)
+                        .bold()
+                }
+            }
             
+            ScopeView(ObservableViewModel.self) { _ , projected in
+                TextField("Text", text: projected.text)
+                    .textFieldStyle(.roundedBorder)
+            }
         }
         .padding()
         .background { Color.random() }
+        .onChange(of: \.count, in: ObservableViewModel.self) { oldValue, newValue in
+            print("Count changed form \(oldValue) to \(newValue)")
+        }
     }
 }
 
@@ -41,9 +59,29 @@ struct ObservableView: View {
 struct ObservableObjectView: View {
     var body: some View {
         VStack {
+            ScopeView(ObservableObjectViewModel.self) { viewModel, _ in
+                Button {
+                    viewModel.count += 1
+                    print("Did tap Observable: \(viewModel.count)")
+                } label: {
+                    Text(viewModel.text)
+                        .foregroundStyle(.white)
+                        .bold()
+                }
+            }
             
+            ScopeView(ObservableObjectViewModel.self) { _ , projected in
+                TextField("Text", text: projected.text)
+                    .textFieldStyle(.roundedBorder)
+            }
         }
         .padding()
         .background { Color.random() }
+        .onChange(of: \.count, in: ObservableObjectViewModel.self) { oldValue, newValue in
+            print("Count changed form \(oldValue) to \(newValue)")
+        }
+        .onReceive(of: \.$count, in: ObservableObjectViewModel.self) { newValue in
+            print("Count received: \(newValue)")
+        }
     }
 }
